45ab429267c2f4296f8993abf231b24026224d4e
Vignesh Muralidharan <vignesh.em@swiggy.in>
2023-05-09 10:29:00 +0530

    Load images on image view

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
 
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    kapt ("com.github.bumptech.glide:compiler:4.14.2")
+    implementation ("com.github.bumptech.glide:avif-integration:4.14.2")
+
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- app/src/main/java/com/example/avifsample/MainActivity.kt
+++ app/src/main/java/com/example/avifsample/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.avifsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.avifsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        loadImage(binding.avifImage, "f_avif")
+        loadImage(binding.webpImage, "f_webp")
     }
+
+    private fun loadImage(imageView: ImageView, formatParam: String) {
+        Glide.with(this).load(buildImageUrl(formatParam)).centerCrop()
+            .placeholder(R.drawable.ic_launcher_foreground).into(imageView)
+    }
+
+    private fun buildImageUrl(formatParam: String) =
+        "https://res.cloudinary.com/swiggy/image/upload/c_fill,$formatParam,q_auto,h_375,w_302/v1674029842/PC_Creative%20refresh/3D_bau/banners_new/Vada.png".also {
+            Log.d(TAG, "buildImageUrl: $it")
+        }
 }
+
+private const val TAG = "MainActivity"
