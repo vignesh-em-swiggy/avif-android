bcea6e9083279ea6e44f0dcf2ef0446e6a972cfc
Vignesh Muralidharan <vignesh.em@swiggy.in>
2023-05-16 11:54:03 +0530

    Update Sample URL

--- app/src/main/java/com/example/avifsample/MainActivity.kt
+++ app/src/main/java/com/example/avifsample/MainActivity.kt
@@ -29,8 +29,8 @@ class MainActivity : AppCompatActivity() {
         .format(DecodeFormat.PREFER_RGB_565)
         .disallowHardwareConfig()
 
-    // food bb icon https://res.cloudinary.com/swiggy/image/upload/c_fit,fl_lossy,f_webp,q_auto:eco/rng/md/carousel/production/z5kgwcyecjebueopxcbs
-    // food nav tile https://res.cloudinary.com/swiggy/image/upload/c_fill,fl_lossy,f_webp,q_auto:eco,h_231,w_189/rng/md/carousel/production/uycdjcbljscpflwcjeob
+    // food bb icon https://res.cloudinary.com/swiggy/image/upload/f_webp/rng/md/carousel/production/z5kgwcyecjebueopxcbs
+    // food nav tile https://res.cloudinary.com/swiggy/image/upload/f_webp/rng/md/carousel/production/uycdjcbljscpflwcjeob
     private fun buildImageUrl(formatParam: String) =
         "https://res.cloudinary.com/swiggy/image/upload/$formatParam/rng/md/carousel/production/uycdjcbljscpflwcjeob".also {
             Log.d(TAG, "buildImageUrl: $it")
